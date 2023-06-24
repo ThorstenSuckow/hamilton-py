ddf77ac8ed53cf0b081a29cedd0cc6b273b0b3be
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-06-24 23:46:11 +0200

    refactor: constructor accepts adjacency matrix
    updated vertices() to return list of vertices

--- test_graph.py
+++ test_graph.py
@@ -8,11 +8,7 @@ def test_graph():
         [0, 0, 1]
     ])
 
-    assert g.vertices() == [
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1]
-    ]
+    assert g.vertices() == [1, 2, 3]
 
     assert g.edges() == [
         [1, 1], [2, 2], [3, 3]
@@ -25,9 +21,28 @@ def test_graph():
         [1, 0, 1, 0]
     ])
 
-
     assert g.edges() == [
         [1, 2], [1, 4], [2, 1], [2, 3], [3, 2], [3, 4], [4, 1], [4, 3]
     ]
+
+    assert g.vertices() == [
+        1, 2, 3, 4
+    ]
+
+    g = Graph([
+        [0, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0]
+    ])
+
+    assert g.edges() == [
+        [2, 4], [3, 2]
+    ]
+
+    assert g.vertices() == [
+       2, 3, 4
+    ]
+
 
     pass
